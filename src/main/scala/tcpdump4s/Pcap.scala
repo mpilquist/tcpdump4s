ccef96f5bd11cdca61813985cb7b06756698eb9d
Michael Pilquist <mpilquist@gmail.com>
2022-07-31 10:42:13 -0400

    Refactored

--- src/main/scala/tcpdump4s/Pcap.scala
+++ src/main/scala/tcpdump4s/Pcap.scala
@@ -22,30 +22,31 @@ object Pcap:
     val rc = getnameinfo(ptrSa, (!ptrSa).len, host, 256, serv, 256, NI_NUMERICHOST)
     if rc == 0 then Some(fromNullableString(host)) else None
 
-  private def fromPcapIf(ptr: Ptr[pcap_if]): List[Interface] = ???
+  private def fromPcapIf(ptr: Ptr[pcap_if]): List[Interface] =
+    val bldr = List.newBuilder[Interface]
+    var ptrPcapIf = ptr
+    while ptrPcapIf ne null do
+      val entry = !ptrPcapIf
+      bldr += Interface(fromCString(entry.name), fromNullableString(entry.description), fromPcapAddr(entry.addresses))
+      ptrPcapIf = entry.next
+    bldr.result()
+
   private def fromPcapAddr(ptr: Ptr[pcap_addr]): List[SockAddr] =
-    val ab = List.newBuilder[SockAddr]
+    val bldr = List.newBuilder[SockAddr]
     var addr = ptr
     while addr ne null do
       val ptrSa: Ptr[sockaddr] = (!addr).addr
       val family = (!ptrSa).family.toInt
       if family == AF_INET || family == AF_INET6
-      then getIpString(ptrSa).foreach(s => ab += SockAddr(s))
+      then getIpString(ptrSa).foreach(s => bldr += SockAddr(s))
       addr = (!addr).next
-    ab.result()
+    bldr.result()
 
   def interfaces: List[Interface] =
     Zone { implicit z =>
       val p = stackalloc[Ptr[pcap_if]](1)
       val errbuf = makeErrorBuffer
       val rc = pcap_findalldevs(p, errbuf)
-      if rc == 0 then
-        val bldr = List.newBuilder[Interface]
-        var q = !p
-        while q ne null do
-          val entry = !q
-          bldr += Interface(fromCString(entry.name), fromNullableString(entry.description), fromPcapAddr(entry.addresses))
-          q = entry.next
-        bldr.result()
+      if rc == 0 then fromPcapIf(!p)
       else throw new RuntimeException(s"pcap_findalldevs failed with error code $rc: ${fromCString(errbuf)}")
     }
