1ac42eb2dcc7406d017ba06ea27ed44856e83548
Michael Pilquist <mpilquist@gmail.com>
2022-08-06 14:06:12 -0400

    TCP decoding

--- src/main/scala/tcpdump4s/DecodedPacket.scala
+++ src/main/scala/tcpdump4s/DecodedPacket.scala
@@ -6,6 +6,7 @@ import scodec.bits.ByteVector
 import fs2.protocols.ethernet.{EtherType, EthernetFrameHeader}
 //import fs2.protocols.ip.IpHeader
 import fs2.protocols.ip.{Ipv4Header, Ipv6Header}
+import fs2.protocols.ip.tcp.TcpHeader
 import fs2.protocols.ip.udp.DatagramHeader
 
 opaque type IpHeader = Either[Ipv4Header, Ipv6Header]
@@ -20,6 +21,7 @@ object IpHeader:
 enum DecodedPacketPart:
   case Ethernet(value: EthernetFrameHeader)
   case Ip(value: IpHeader)
+  case Tcp(value: TcpHeader)
   case Udp(value: DatagramHeader)
   case UnsupportedEtherType(ethertype: Option[Int])
   case UnsupportedIpProtocol(protocol: Int)
@@ -27,6 +29,7 @@ enum DecodedPacketPart:
   def render: String = this match
     case Ethernet(v) => f"Ethernet: src: ${v.source}%-20s dst: ${v.destination}"
     case Ip(v)       => f"IP:       src: ${v.sourceIp}%-20s dst: ${v.destinationIp}"
+    case Tcp(v)      => f"TCP:      src: ${v.sourcePort}%-20s dst: ${v.destinationPort}\n          seq: ${v.sequenceNumber}%-20d ack: ${v.ackNumber}%-20d win: ${v.windowSize}"
     case Udp(v)      => f"UDP:      src: ${v.sourcePort}%-20s dst: ${v.destinationPort}"
     case UnsupportedEtherType(etherType) => s"""Unsupported ether type ${etherType.getOrElse("n/a")}"""
     case UnsupportedIpProtocol(p) => s"Unsupported IP protocol $p"
@@ -56,12 +59,16 @@ object DecodedPacket:
     decoder.flatMap { ipHeader =>
       val part = DecodedPacketPart.Ip(ipHeader)
       ipHeader.protocol match
+        case 6 => decodeTcpHeader(part :: acc)
         case 17 => decodeUdpDatagram(part :: acc)
         case o => Decoder.pure(DecodedPacketPart.UnsupportedIpProtocol(o) :: part :: acc)
     }
 
   private def decodeUdpDatagram(acc: List[DecodedPacketPart]): Decoder[List[DecodedPacketPart]] =
     DatagramHeader.codec.map(udp => DecodedPacketPart.Udp(udp) :: acc)
+
+  private def decodeTcpHeader(acc: List[DecodedPacketPart]): Decoder[List[DecodedPacketPart]] =
+    TcpHeader.codec.map(tcp => DecodedPacketPart.Tcp(tcp) :: acc)
 
   def decode(bytes: ByteVector) =
     decoder.decode(bytes.bits)
